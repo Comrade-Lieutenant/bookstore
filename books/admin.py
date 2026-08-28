2760a52e45e6fe4103471e7278d732c1e3d68e5c
JzKrpts <261347858+JzKrpts@users.noreply.github.com>
2026-08-28 10:12:30 -0700

    feat: reviews

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
+
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInline,
+    ]
+
     list_display = (
         "title",
         "author",
@@ -11,4 +20,4 @@ class BookAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
